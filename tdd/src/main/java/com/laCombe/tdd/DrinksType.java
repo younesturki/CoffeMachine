82f9f648b60d7ca8da2d657e133b7b7d991d49cb
youne <younesturki@live.fr>
2020-04-19 17:33:23 +0200

    Third Iteration

--- tdd/src/main/java/com/laCombe/tdd/DrinksType.java
+++ tdd/src/main/java/com/laCombe/tdd/DrinksType.java
@@ -4,12 +4,20 @@ public enum DrinksType {
 	
 	TEA("T",0.4), COFFEE("C",0.6), CHOCOLATE("H",0.5), ORANGE("O",0.6);
 	
-	public final String value;
-	public double amount;
+	private final String value;
+	private double amount;
 	
 	private DrinksType(String value, double amount) {
 		this.value = value;
 		this.amount = amount;
+	}
+	
+	public String getValue() {
+		return value;
+	}
+	
+	public double getAmount() {
+		return amount;
 	}
 
 }
